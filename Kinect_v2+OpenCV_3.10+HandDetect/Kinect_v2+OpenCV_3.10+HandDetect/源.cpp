2d012ab9f283d8e30970672d2d28dfc9d5318411
朱鹮 <haffman@live.com>
2016-08-03 12:12:09 +0800

    单线程work

--- Kinect_v2+OpenCV_3.10+HandDetect/Kinect_v2+OpenCV_3.10+HandDetect/源.cpp
+++ Kinect_v2+OpenCV_3.10+HandDetect/Kinect_v2+OpenCV_3.10+HandDetect/源.cpp
@@ -14,10 +14,11 @@ using namespace cv;
 using namespace std;
 const string  get_name(int n);    //此函数判断出关节点的名字
 bool send(int ctl,int x, int y);
+void work();
 void tructbar();
 void iniKinect();
-int myBodyCount = 0, height = 0, width = 0;
-
+int height = 0, width = 0;
+int speed=0;
 IKinectSensor   * mySensor = nullptr;
 
 IBodyFrameSource    * myBodySource = nullptr;
@@ -32,54 +33,65 @@ IBodyFrame  * myBodyFrame = nullptr;
 IDepthFrame * myDepthFrame = nullptr;
 
 Mat img16, img8;
+HandState leftHandState, rightHandState;
 ////////////////////////////////////
 typedef struct tructvar { int min, max;}tructvar;
 tructvar bodydepth;
 Point3d lefthand;
+Point3d zerolefthand;
 Point arduino;
 CSerial serial;
 int main(void)
 {
 	
-	if(serial.OpenSerialPort(_T("COM2:"), 9600, 8, 1)==false) return -1;
+	//if(serial.OpenSerialPort(_T("COM2:"), 9600, 8, 1)==false) return -1;
 	iniKinect();
 	tructbar();
-	while(send(STOP, 0, 0)) Sleep(300);
+	//while(send(STOP, 0, 0)) Sleep(300);
     while (1)
     {
-        while (myDepthReader->AcquireLatestFrame(&myDepthFrame) != S_OK);
-        myDepthFrame->CopyFrameDataToArray(width * height,(UINT16 *)img16.data);
-        img16.convertTo(img8,CV_8UC1,255.0 / 4500);
-        imshow("Depth Img", img8);  //深度图像的转化及显示
+        //while (myDepthReader->AcquireLatestFrame(&myDepthFrame) != S_OK);
+        //myDepthFrame->CopyFrameDataToArray(width * height,(UINT16 *)img16.data);
+        //img16.convertTo(img8,CV_8UC1,255.0 / 4500);
+        //imshow("Depth Img", img8);  //深度图像的转化及显示
 
         while (myBodyReader->AcquireLatestFrame(&myBodyFrame) != S_OK);
 
-        int myBodyCount = 0;        
-        IBody   ** bodyArr = nullptr;
-        myBodySource->get_BodyCount(&myBodyCount);
-        bodyArr = new IBody *[myBodyCount];
-        for (int i = 0; i < myBodyCount; i++)   //bodyArr的初始化
-            bodyArr[i] = nullptr;       
+        //int myBodyCount = 0;        
+        //IBody   ** bodyArr = nullptr;
+        //myBodySource->get_BodyCount(&myBodyCount);
+        //bodyArr = new IBody *[myBodyCount];
+        //for (int i = 0; i < myBodyCount; i++)   //bodyArr的初始化
+         //   bodyArr[i] = nullptr;       
+		IBody   * bodyArr[BODY_COUNT] = {0};
 
-        myBodyFrame->GetAndRefreshBodyData(myBodyCount,bodyArr);
-        for (int i = 0; i < myBodyCount; i++)   //遍历6个人(可能用不完)
+        myBodyFrame->GetAndRefreshBodyData(BODY_COUNT,bodyArr);
+        for (int i = 0; i < BODY_COUNT; i++)   //遍历6个人(可能用不完)
         {
             BOOLEAN     result = false;
             if (bodyArr[i]->get_IsTracked(&result) == S_OK && result)   //判断此人是否被侦测到
             {
                 cout << "Body " << i << " tracked!" << endl;
-
-                int count   = 0;
                 Joint   jointArr[JointType_Count];
                 bodyArr[i]->GetJoints(JointType_Count,jointArr);    //获取此人的关节数据
+				
+				//判断距离合法否
+				if (!(jointArr[0].Position.Z <= bodydepth.max &&jointArr[0].Position.Z >= bodydepth.min)) continue;
+
+				leftHandState = HandState_Unknown;
+				bodyArr[i]->get_HandLeftState(&leftHandState);
+				rightHandState = HandState_Unknown;
+				bodyArr[i]->get_HandRightState(&rightHandState);//更新左右手数据
+
+
                 for (int j = 0; j < JointType_Count; j++)
                 {
-                    if (jointArr[j].TrackingState != TrackingState_Tracked) //将确定侦测到的关节显示出来
+                    if (jointArr[j].TrackingState == TrackingState_NotTracked) //将确定侦测到的关节显示出来
                         continue;
                     string  rt = get_name(jointArr[j].JointType);   //获取关节的名字
                     if (rt != "NULL")   //输出关节信息
                     {
-                        count++;
+
                         cout << "   " << rt << " tracked" << endl;
 						if (rt == "Left hand")
 						{
@@ -91,9 +103,12 @@ int main(void)
                 }
             }
         }
+		work();
+		
+
+		
         if(myDepthFrame!=nullptr)myDepthFrame->Release();
 		if (myBodyFrame!= nullptr)myBodyFrame->Release();
-        delete[] bodyArr;
 
         if (waitKey(30) == VK_ESCAPE)
             break;
@@ -140,6 +155,25 @@ bool send(int ctl,int x, int y)
 	
 }
 
+void work()
+{
+	////////////ini
+	if (rightHandState == HandState_Closed && leftHandState == HandState_Closed)
+	{
+		send(CENTER, 0, 0);
+		arduino = Point(11000, 16000);
+		zerolefthand = lefthand;
+	}
+	////////////
+	if (leftHandState == HandState_NotTracked)
+	{
+		send(ZERO, 0, 0);
+		arduino = Point(0, 0);
+	}
+	//////////////
+	send(NORMAL, (lefthand.x - zerolefthand.x)*speed, (lefthand.y - zerolefthand.y)*speed);
+}
+
 void tructbar()
 {
 	namedWindow("body",1);
@@ -154,7 +188,6 @@ void iniKinect()
 
 	mySensor->get_BodyFrameSource(&myBodySource);
 	myBodySource->OpenReader(&myBodyReader);
-	myBodySource->get_BodyCount(&myBodyCount);
 
 
 	mySensor->get_DepthFrameSource(&myDepthSource);
