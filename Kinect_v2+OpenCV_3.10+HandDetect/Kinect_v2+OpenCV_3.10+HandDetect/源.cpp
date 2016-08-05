e2e5dc4f9a53a43825cb1294b5e146dd458b526b
haffm <haffm@SU>
2016-08-05 09:46:08 +0800

    增加待机功能

--- Kinect_v2+OpenCV_3.10+HandDetect/Kinect_v2+OpenCV_3.10+HandDetect/源.cpp
+++ Kinect_v2+OpenCV_3.10+HandDetect/Kinect_v2+OpenCV_3.10+HandDetect/源.cpp
@@ -10,15 +10,24 @@
 #define CENTER 2
 #define ROUND 3
 #define STOP 4
+#define SLEEP 5
+#define YES	 1
+#define NO 0
 using namespace cv;
 using namespace std;
 const string  get_name(int n);    //此函数判断出关节点的名字
 bool send(int ctl,int x, int y);
+void reflashcameradata();
 void work();
 void tructbar();
 void iniKinect();
+void mode_setup();
+void mode_sleep();
+int is_san();
 int height = 0, width = 0;
 int speed=0;
+int is_human = NO;
+int is_setup = NO;
 IKinectSensor   * mySensor = nullptr;
 
 IBodyFrameSource    * myBodySource = nullptr;
@@ -37,11 +46,11 @@ HandState leftHandState, rightHandState;
 ////////////////////////////////////
 typedef struct tructvar { int min, max;}tructvar;
 tructvar bodydepth;
-Point3d lefthand;
+Point3d lefthand,righthand;
 Point3d zerolefthand;
-Point arduino;
 CSerial serial;
-int main(void)
+///////////////////////////////////////
+int main()
 {
 	
 	//if(serial.OpenSerialPort(_T("COM2:"), 9600, 8, 1)==false) return -1;
@@ -54,62 +63,37 @@ int main(void)
         //myDepthFrame->CopyFrameDataToArray(width * height,(UINT16 *)img16.data);
         //img16.convertTo(img8,CV_8UC1,255.0 / 4500);
         //imshow("Depth Img", img8);  //深度图像的转化及显示
-
-        while (myBodyReader->AcquireLatestFrame(&myBodyFrame) != S_OK);
-
-        //int myBodyCount = 0;        
-        //IBody   ** bodyArr = nullptr;
-        //myBodySource->get_BodyCount(&myBodyCount);
-        //bodyArr = new IBody *[myBodyCount];
-        //for (int i = 0; i < myBodyCount; i++)   //bodyArr的初始化
-         //   bodyArr[i] = nullptr;       
-		IBody   * bodyArr[BODY_COUNT] = {0};
-
-        myBodyFrame->GetAndRefreshBodyData(BODY_COUNT,bodyArr);
-        for (int i = 0; i < BODY_COUNT; i++)   //遍历6个人(可能用不完)
-        {
-            BOOLEAN     result = false;
-            if (bodyArr[i]->get_IsTracked(&result) == S_OK && result)   //判断此人是否被侦测到
-            {
-                cout << "Body " << i << " tracked!" << endl;
-                Joint   jointArr[JointType_Count];
-                bodyArr[i]->GetJoints(JointType_Count,jointArr);    //获取此人的关节数据
-				
-				//判断距离合法否
-				if (!(jointArr[0].Position.Z <= bodydepth.max &&jointArr[0].Position.Z >= bodydepth.min)) continue;
-
-				leftHandState = HandState_Unknown;
-				bodyArr[i]->get_HandLeftState(&leftHandState);
-				rightHandState = HandState_Unknown;
-				bodyArr[i]->get_HandRightState(&rightHandState);//更新左右手数据
-
-
-                for (int j = 0; j < JointType_Count; j++)
-                {
-                    if (jointArr[j].TrackingState == TrackingState_NotTracked) //将确定侦测到的关节显示出来
-                        continue;
-                    string  rt = get_name(jointArr[j].JointType);   //获取关节的名字
-                    if (rt != "NULL")   //输出关节信息
-                    {
-
-                        cout << "   " << rt << " tracked" << endl;
-						if (rt == "Left hand")
-						{
-							lefthand = Point3d((int)(jointArr[j].Position.X * 1000000), (int)(jointArr[j].Position.Y * 1000000), (int)(jointArr[j].Position.Z * 1000));
-							cout << "lefthand" << lefthand.x << "   " << lefthand.y << "   "<< lefthand.z<<endl;
-						}
-                           
-                    }
-                }
-            }
-        }
-		work();
+		reflashcameradata();
+		if (is_setup == YES)
+		{
+			if (is_human == NO)
+			{
+				send(SLEEP, 0, 0);
+				is_setup = NO;
+			}
+			else
+			{
+				if(is_san()==YES)
+				{
+					send(ROUND, (lefthand.x - zerolefthand.x)*speed, (lefthand.y - zerolefthand.y)*speed);
+				}
+				else send(NORMAL, (lefthand.x - zerolefthand.x)*speed, (lefthand.y - zerolefthand.y)*speed);
+			}
+		}
+		else
+		{
+			if (is_human == YES)
+			{
+				if (rightHandState == HandState_Closed && leftHandState == HandState_Closed)
+				{
+					send(CENTER, 0, 0);
+					zerolefthand = lefthand;
+					is_setup = YES;
+				}
+			}
+		}
 		
-
-		
-        if(myDepthFrame!=nullptr)myDepthFrame->Release();
-		if (myBodyFrame!= nullptr)myBodyFrame->Release();
-
+        
         if (waitKey(30) == VK_ESCAPE)
             break;
         //Sleep(1000);    //为避免数据刷太快，每秒钟更新一次
@@ -155,23 +139,59 @@ bool send(int ctl,int x, int y)
 	
 }
 
-void work()
+void reflashcameradata()
 {
-	////////////ini
-	if (rightHandState == HandState_Closed && leftHandState == HandState_Closed)
+	while (myBodyReader->AcquireLatestFrame(&myBodyFrame) != S_OK);
+
+	//int myBodyCount = 0;        
+	//IBody   ** bodyArr = nullptr;
+	//myBodySource->get_BodyCount(&myBodyCount);
+	//bodyArr = new IBody *[myBodyCount];
+	//for (int i = 0; i < myBodyCount; i++)   //bodyArr的初始化
+	//   bodyArr[i] = nullptr;       
+	IBody   * bodyArr[BODY_COUNT] = { 0 };
+
+	myBodyFrame->GetAndRefreshBodyData(BODY_COUNT, bodyArr);
+	for (int i = 0; i < BODY_COUNT; i++)   //遍历6个人(可能用不完)
 	{
-		send(CENTER, 0, 0);
-		arduino = Point(11000, 16000);
-		zerolefthand = lefthand;
+		BOOLEAN     result = false;
+		if (bodyArr[i]->get_IsTracked(&result) == S_OK && result)   //判断此人是否被侦测到
+		{
+			cout << "Body " << i << " tracked!" << endl;
+			Joint   jointArr[JointType_Count];
+			bodyArr[i]->GetJoints(JointType_Count, jointArr);    //获取此人的关节数据
+
+																 //判断距离合法否
+			if (!(jointArr[0].Position.Z <= bodydepth.max &&jointArr[0].Position.Z >= bodydepth.min)) continue;
+
+			leftHandState = HandState_Unknown;
+			bodyArr[i]->get_HandLeftState(&leftHandState);
+			rightHandState = HandState_Unknown;
+			bodyArr[i]->get_HandRightState(&rightHandState);//更新左右手数据
+
+
+			for (int j = 0; j < JointType_Count; j++)
+			{
+				if (jointArr[j].TrackingState == TrackingState_NotTracked) //将确定侦测到的关节显示出来
+					continue;
+				string  rt = get_name(jointArr[j].JointType);   //获取关节的名字
+				if (rt != "NULL")   //输出关节信息
+				{
+
+					cout << "   " << rt << " tracked" << endl;
+					if (rt == "Left hand")
+					{
+						lefthand = Point3d((int)(jointArr[j].Position.X * 1000000), (int)(jointArr[j].Position.Y * 1000000), (int)(jointArr[j].Position.Z * 1000));
+						cout << "lefthand" << lefthand.x << "   " << lefthand.y << "   " << lefthand.z << endl;
+					}
+
+				}
+			}
+		}
 	}
-	////////////
-	if (leftHandState == HandState_NotTracked)
-	{
-		send(ZERO, 0, 0);
-		arduino = Point(0, 0);
-	}
-	//////////////
-	send(NORMAL, (lefthand.x - zerolefthand.x)*speed, (lefthand.y - zerolefthand.y)*speed);
+	if (myDepthFrame != nullptr)myDepthFrame->Release();
+	if (myBodyFrame != nullptr)myBodyFrame->Release();
+
 }
 
 void tructbar()
@@ -201,5 +221,21 @@ void iniKinect()
 
 	img16.create(height, width, CV_16UC1);//为显示深度图像做准备
 	img8.create(height, width, CV_8UC1);
+}
+
+void mode_setup()
+{
+	
+}
+
+
+
+int is_san()
+{
+	if (rightHandState == HandState_Closed)
+	{
+		return YES;
+	}
+	else return NO;
 }
 
